19735a0f859202f816cbc88b1f1bac7768ed9d7a
Max Rosenthal <mxrosenthal@yahoo.com>
2020-03-10 18:58:54 +0000

    initial commit. able to read request body. need to formulate some logic.

--- index.js
+++ index.js
@@ -1,66 +1,67 @@
-const bodyParser = require('body-parser')
-const express = require('express')
-const logger = require('morgan')
-const app = express()
+const bodyParser = require('body-parser');
+const express = require('express');
+const logger = require('morgan');
+const app = express();
 const {
   fallbackHandler,
   notFoundHandler,
   genericErrorHandler,
   poweredByHandler
-} = require('./handlers.js')
+} = require('./handlers.js');
 
 // For deployment to Heroku, the port needs to be set using ENV, so
 // we check for the port number in process.env
-app.set('port', (process.env.PORT || 9001))
+app.set('port', process.env.PORT || 9001);
 
-app.enable('verbose errors')
+app.enable('verbose errors');
 
-app.use(logger('dev'))
-app.use(bodyParser.json())
-app.use(poweredByHandler)
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(poweredByHandler);
 
 // --- SNAKE LOGIC GOES BELOW THIS LINE ---
 
 // Handle POST request to '/start'
 app.post('/start', (request, response) => {
   // NOTE: Do something here to start the game
-
+  const info = request.body;
   // Response data
   const data = {
-    color: '#DFFF00',
-  }
+    color: '#DFFF00'
+  };
 
-  return response.json(data)
-})
+  return response.json(data);
+});
 
 // Handle POST request to '/move'
 app.post('/move', (request, response) => {
   // NOTE: Do something here to generate your move
+  const info = request.body;
 
   // Response data
   const data = {
-    move: 'up', // one of: ['up','down','left','right']
-  }
+    move: 'up' // one of: ['up','down','left','right']
+  };
 
-  return response.json(data)
-})
+  return response.json(data);
+});
 
 app.post('/end', (request, response) => {
   // NOTE: Any cleanup when a game is complete.
-  return response.json({})
-})
+  return response.json({});
+});
 
 app.post('/ping', (request, response) => {
   // Used for checking if this snake is still alive.
   return response.json({});
-})
+});
 
 // --- SNAKE LOGIC GOES ABOVE THIS LINE ---
 
-app.use('*', fallbackHandler)
-app.use(notFoundHandler)
-app.use(genericErrorHandler)
+app.use('*', fallbackHandler);
+app.use(notFoundHandler);
+app.use(genericErrorHandler);
 
 app.listen(app.get('port'), () => {
-  console.log('Server listening on port %s', app.get('port'))
-})
+  console.log('Server listening on port %s', app.get('port'));
+});
