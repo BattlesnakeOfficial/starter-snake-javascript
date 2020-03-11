c0be4019a35646f3ee1b6ed8a8478aee5a37bae4
cassidyq <cassidy@referralsaasquatch.com>
2020-03-10 18:13:38 -0700

    Updated functions to be getOccupiedSquares that includes our snake's body and getOpenSquares that returns all available moves

--- getOpenSquares-helpers.js
+++ getOpenSquares-helpers.js
@@ -1,39 +1,52 @@
-const getOpponentsPositions = function(info) {
+const getOccupiedSquares = function(info) {
+  //get snakes on board
   const snakes = info.board.snakes;
+  //get your body coords
+  const ourSnake = info.you.body;
   let positions = [];
+  //loop through snakes on board and their bodies to add filled squares to positions
   for (const snake of snakes) {
     for (const square of snake.body) {
       positions.push(JSON.stringify(square));
     }
   }
+  //add coords of your body to positions 
+  for (const square of ourSnake) {
+    positions.push(JSON.stringify(square));
+  }
   return positions;
 }
 
-
+// potential upgrade for this function would be to take into consideration if a square is occupied by a tail that will move out of our way
 const getOpenSquares = function (info) {
   let openSquares = [true, true, true, true];
   const head = info.you.body[0];
-  const neck = info.you.body[1];
-  const filledSquares = getOpponentsPositions(info);
-
+  const filledSquares = getOccupiedSquares(info);
+  
+  // get all potential next moves from current position
   const north = {"x": head.x,"y": head.y - 1}
   const east = {"x": head.x + 1,"y": head.y}
   const south = {"x": head.x, "y": head.y + 1}
   const west = {"x": head.x - 1, "y": head.y}
 
-  if (JSON.stringify(north) === JSON.stringify(neck) || north.y < 0 || filledSquares.includes(JSON.stringify(north))) {
+  // if the square is occupied set value to false for that direction
+  if (north.y < 0 || filledSquares.includes(JSON.stringify(north))) {
     openSquares[0] = false;
   } 
-  if (JSON.stringify(east) === JSON.stringify(neck) || east.x > info.board.width - 1 || filledSquares.includes(JSON.stringify(east))) {
+  if (east.x > info.board.width - 1 || filledSquares.includes(JSON.stringify(east))) {
     openSquares[1] = false;
   } 
-  if (JSON.stringify(south) === JSON.stringify(neck) || south.y > info.board.length - 1 || filledSquares.includes(JSON.stringify(south))) {
+  if (south.y > info.board.length - 1 || filledSquares.includes(JSON.stringify(south))) {
     openSquares[2] = false;
   } 
-  if (JSON.stringify(west) === JSON.stringify(neck) || west.x < 0 || filledSquares.includes(JSON.stringify(north))) {
+  if (west.x < 0 || filledSquares.includes(JSON.stringify(west))) {
     openSquares[3] = false;
   }  
-  
   return openSquares;
 }
+
+module.exports = {
+  getOpenSquares,
+  getOccupiedSquares
+};
 
