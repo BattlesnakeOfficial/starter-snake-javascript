28b7c57ed28034bbdea2e3d39d818b15b20c16ee
Jon Langlois <jonathan-langlois@live.ca>
2020-03-10 20:45:55 -0700

    update move route

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ const getDanger = require('./getDanger.js');
 
 // For deployment to Heroku, the port needs to be set using ENV, so
 // we check for the port number in process.env
-app.set('port', process.env.PORT || 9001);
+// app.set('port', process.env.PORT || 9001);
+app.set('port', 9001);
 app.enable('verbose errors');
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -59,15 +60,24 @@ app.post('/test', (request, response) => {
 // Handle POST request to '/move'
 app.post('/move', (request, response) => {
   // NOTE: Do something here to generate your move
+  // const info = request.body;
+  // const head = info.you.body[0];
+  // const foodArray = info.board.food;
+  // const hunger = info.you.health < 40;
+  // const possibleMoves = getOpenSquares(info);
+  // let rankedMoves = rankFood(head, foodArray, possibleMoves);
+  // rankedMoves = rankDanger(info, rankedMoves);
+  // const move = chooseDirection(rankedMoves, hunger);
+
   const info = request.body;
-  const head = info.you.body[0];
-  const foodArray = info.board.food;
-  const hunger = info.you.health < 40;
-  const possibleMoves = getOpenSquares(info);
-  let rankedMoves = rankFood(head, foodArray, possibleMoves);
-  rankedMoves = rankDanger(info, rankedMoves);
-  const move = chooseDirection(rankedMoves, hunger);
-  const data = { move };
+  // Response data
+
+  const directions = rankFood(info.you.body[0], info.board.food, info);
+
+  const dangerDirections = getDanger(info, directions, getOpenSquares(info));
+
+  const direction = chooseDirection(info, dangerDirections);
+  const data = { move:direction };
   return response.json(data);
 });
 
