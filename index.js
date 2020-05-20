c53be52c8c62f6d468f8012e1e6518970437f951
Brad Van Vugt <1531419+bvanvugt@users.noreply.github.com>
2020-05-19 17:08:33 -0700

    Rewrite and simplify for API v1.

--- index.js
+++ index.js
@@ -1,75 +1,52 @@
 const bodyParser = require('body-parser')
 const express = require('express')
-const logger = require('morgan')
+
+const PORT = process.env.PORT || 3000
+
 const app = express()
-const {
-  fallbackHandler,
-  notFoundHandler,
-  genericErrorHandler,
-  poweredByHandler
-} = require('./handlers.js')
-
-// For deployment to Heroku, the port needs to be set using ENV, so
-// we check for the port number in process.env
-app.set('port', (process.env.PORT || 9001))
-
-app.enable('verbose errors')
-
-app.use(logger('dev'))
 app.use(bodyParser.json())
-app.use(poweredByHandler)
 
-// --- SNAKE LOGIC GOES BELOW THIS LINE ---
+app.get('/', handleIndex)
+app.post('/start', handleStart)
+app.post('/move', handleMove)
+app.post('/end', handleEnd)
 
-//  This function is called everytime your snake is entered into a game.
-//  cherrypy.request.json contains information about the game that's about to be played.
-// TODO: Use this function to decide how your snake is going to look on the board.
-app.post('/start', (request, response) => {
-  console.log("START");
+app.listen(PORT, () => console.log(`Example app listening at http://127.0.0.1:${PORT}`))
 
-  // Response data
-  const data = {
-    color: '#888888',
-    headType: "regular",
-    tailType: "regular"
+
+function handleIndex(request, response) {
+  var battlesnakeInfo = {
+    apiversion: '1',
+    author: '',
+    color: '#880000',
+    head: 'tongue',
+    tail: 'curled'
   }
+  response.status(200).json(battlesnakeInfo)
+}
 
-  return response.json(data)
-})
+function handleStart(request, response) {
+  var gameData = request.body
 
-// This function is called on every turn of a game. It's how your snake decides where to move.
-// Valid moves are "up", "down", "left", or "right".
-// TODO: Use the information in cherrypy.request.json to decide your next move.
-app.post('/move', (request, response) => {
-  var data = request.body;
+  console.log('START')
+  response.status(200).send('ok')
+}
 
-  // Choose a random direction to move in
-  possible_moves = ["up", "down", "left", "right"]
-  var choice = Math.floor(Math.random() * possible_moves.length);
-  var snake_move = possible_moves[choice];
+function handleMove(request, response) {
+  var gameData = request.body
 
-  console.log("MOVE: " + snake_move);
-  return response.json({ move: snake_move })
-})
+  var possibleMoves = ['up', 'down', 'left', 'right']
+  var move = possibleMoves[Math.floor(Math.random() * possibleMoves.length)]
 
-// This function is called when a game your snake was in ends.
-// It's purely for informational purposes, you don't have to make any decisions here.
-app.post('/end', (request, response) => {
-  console.log("END");
-  return response.json({ message: "ok" });
-})
+  console.log('MOVE: ' + move)
+  response.status(200).send({
+    move: move
+  })
+}
 
-// The Battlesnake engine calls this function to make sure your snake is working.
-app.post('/ping', (request, response) => {
-  return response.json({ message: "pong" });
-})
+function handleEnd(request, response) {
+  var gameData = request.body
 
-// --- SNAKE LOGIC GOES ABOVE THIS LINE ---
-
-app.use('*', fallbackHandler)
-app.use(notFoundHandler)
-app.use(genericErrorHandler)
-
-app.listen(app.get('port'), () => {
-  console.log('Server listening on port %s', app.get('port'))
-})
+  console.log('END')
+  response.status(200).send('ok')
+}
